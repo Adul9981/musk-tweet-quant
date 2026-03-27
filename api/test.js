61e9d89885914693f65537d11dc641aa0f6a9eff
Adul9981 <serene77mc@gmail.com>
2026-03-27 16:58:58 +0800

    test: add simple test API endpoint

---
+++ api/test.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  return res.status(200).json({ 
+    message: 'API is working!',
+    timestamp: new Date().toISOString(),
+    user: 'elon',
+    tweetCount: 42
+  });
+}
